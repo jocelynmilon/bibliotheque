33d514adeccc3431dc94e590616abbb15bb7e3c0
jocelyn Milon <jocelyn.milon@gmail.com>
2019-04-28 00:58:07 +0200

    creation de compte-connexion

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "../../service/auth.service";
+import { from } from "rxjs";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  selector: "app-signin",
+  templateUrl: "./signin.component.html",
+  styleUrls: ["./signin.component.scss"]
 })
 export class SigninComponent implements OnInit {
+  signInForm: FormGroup;
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.initForm();
   }
+  initForm() {
+    signInForm: FormGroup;
 
+    this.signInForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: [
+        "",
+        [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]
+      ]
+    });
+  }
+  onSubmit() {
+    const email = this.signInForm.get("email").value;
+    const password = this.signInForm.get("password").value;
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(["/books"]);
+      },
+      error => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
