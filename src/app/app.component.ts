44f3bc83f4abe5d12856ef2ecf7a4624c91ddc96
jocelyn Milon <jocelyn.milon@gmail.com>
2019-04-27 20:06:29 +0200

    mis en place de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'bibliotheque';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyAEh9WJ5ksWnq2Sno5zW34-pQhXBHXFfc4",
+      authDomain: "bibliotheque-91e95.firebaseapp.com",
+      databaseURL: "https://bibliotheque-91e95.firebaseio.com",
+      projectId: "bibliotheque-91e95",
+      storageBucket: "bibliotheque-91e95.appspot.com",
+      messagingSenderId: "1071055794008"
+    };
+    firebase.initializeApp(config);
+  }
 }
