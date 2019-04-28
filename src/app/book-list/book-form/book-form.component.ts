233e0df11d805a0633a23ff2f3369858af8f2e7f
jocelyn Milon <jocelyn.milon@gmail.com>
2019-04-29 00:57:50 +0200

    mis en place de la base de donnée

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, Validators } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { BooksService } from "../../service/books.service";
+import { Book } from "../../models/book.model";
 
 @Component({
-  selector: 'app-book-form',
-  templateUrl: './book-form.component.html',
-  styleUrls: ['./book-form.component.scss']
+  selector: "app-book-form",
+  templateUrl: "./book-form.component.html",
+  styleUrls: ["./book-form.component.scss"]
 })
 export class BookFormComponent implements OnInit {
+  bookForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private booksService: BooksService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.initForm();
   }
-
+  initForm() {
+    this.bookForm = this.formBuilder.group({
+      title: ["", Validators.required],
+      author: ["", Validators.required],
+      synopsis: ""
+    });
+  }
+  onSaveBook() {
+    const title = this.bookForm.get("title").value;
+    const author = this.bookForm.get("author").value;
+    const synopsis = this.bookForm.get("synopsis").value;
+    const newBook = new Book(title, author);
+    newBook.synopsis = synopsis;
+    this.booksService.createNewBook(newBook);
+    this.router.navigate(["/books"]);
+  }
 }
